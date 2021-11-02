966206653356f35b12d80954cc8b2ea0e22a4aa1
pavitrakapoor <83365520+pavitrakapoor@users.noreply.github.com>
2021-11-02 13:18:01 +0530

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,34 @@
+export default {
+    products:[
+        {
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numreviews: 10
+    },
+    {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.2,
+        numreviews: 5
+    },
+    {
+        _id: '3',
+        name: 'Best pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 64,
+        brand: 'Nike',
+        rating: 4.4,
+        numreviews: 10
+    }
+    ]
+}
